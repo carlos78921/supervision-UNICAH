91adef1415db3eba808186d4c7f110b7e883d6b9
Byron <byrd_riverat42@unicah.edu>
2025-03-02 16:32:52 -0600

    dgv.enabled = false;

---
+++ PreyectoDesarrollo_unicah/FRMS_DOCENTES/frmDocente.cs
@@ -0,0 +1,49 @@
+using PreyectoDesarrollo_unicah.CLASES;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PreyectoDesarrollo_unicah
+{
+    public partial class frmDocente : Form
+    {
+        public frmDocente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 Login = new Form1();
+            Login.Show();
+        }
+
+        private void frmDocente_Load(object sender, EventArgs e)
+        {
+            lblPersona.Text = $"{ACCIONES_BD.nombre} {ACCIONES_BD.apellido}";
+           // ACCIONES_BD.cargar(dgvDoc,)
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void dgvDoc_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
