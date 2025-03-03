c522fda734e13e670737f2eadfd43431091ce6f4
Byron <byrd_riverat42@unicah.edu>
2025-03-02 23:34:19 -0600

    Validaciones de cierre completo

--- PreyectoDesarrollo_unicah/FRMS_DECANOS/frmReposición.cs
+++ PreyectoDesarrollo_unicah/FRMS_DECANOS/frmReposición.cs
@@ -33,9 +33,8 @@ namespace PreyectoDesarrollo_unicah
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            this.Close();
-            frmDecano menu = new frmDecano();
-            menu.Show();
+            if (MessageBox.Show("Seguro que quieres salirte por completo", "Salida", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
         }
     }
 }
