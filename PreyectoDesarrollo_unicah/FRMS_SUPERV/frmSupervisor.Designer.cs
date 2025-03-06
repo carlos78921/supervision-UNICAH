69b80798beccf1dfff8faff3b1a5ca93bbbbad1a
Lab7-12 <Lab7-12@LAB-12>
2025-03-06 15:48:25 -0600

    Propiedad del timer enabled = true para mostrar la fecha

--- PreyectoDesarrollo_unicah/FRMS_SUPERV/frmSupervisor.Designer.cs
+++ PreyectoDesarrollo_unicah/FRMS_SUPERV/frmSupervisor.Designer.cs
@@ -140,7 +140,7 @@
             label1.AutoSize = true;
             label1.Location = new Point(209, 99);
             label1.Name = "label1";
-            label1.Size = new Size(137, 15);
+            label1.Size = new Size(139, 15);
             label1.TabIndex = 12;
             label1.Text = "REPORTE DE ASISTENCIA";
             // 
@@ -338,6 +338,7 @@
             // 
             // tmrFecha
             // 
+            tmrFecha.Enabled = true;
             tmrFecha.Tick += tmrFecha_Tick;
             // 
             // lblFecha
