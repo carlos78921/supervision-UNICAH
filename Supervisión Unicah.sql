dec77b9d18948b032a09f60b396eeb09765224a3
Byron-Ing <byrd_riverat42@unicah.edu>
2025-03-17 19:05:42 -0600

    Este commit tiene descripción.sql
    Condición
    - Los insert de tabla importada a tabla sql háganlos ustedes, ustedes pueden.

--- Supervisión Unicah.sql
+++ Supervisión Unicah.sql
@@ -3,6 +3,7 @@ go
 
 use Supervision_Unicah 
 go
+--Primero ejecutar los dos comandos para importar, luego coméntenlo para ejecutar todo
 
 set nocount on
 
@@ -26,7 +27,8 @@ go
 
 alter table tabla1$
 add ID_Empleado int foreign key references Nombres_Completos (ID_Empleado)
-	
+
+-- ¿Qué es esto?
 UPDATE T
 SET T.ID_Empleado = NC.ID_Empleado
 FROM Tabla1$ T
@@ -39,6 +41,7 @@ JOIN Nombres_Completos NC
           or UPPER(LTRIM(RTRIM(Nombre4))) = UPPER(LTRIM(RTRIM(Apellido2)))
 	  )
 
+-- ¿Qué es esto?
 INSERT INTO Empleados (ID_Empleado, rol, codigo_empleado)
 SELECT 
     T.ID_Empleado,                           
@@ -109,27 +112,32 @@ BEGIN
 END
 GO
 
-create proc PA_Formato_Asistencia -- Para el Admin
--- Declarar variables ___ para usarlas en el...
+create proc PA_Admin 
 with encryption
 as 
 begin
-	select (Cod_Facultad + ' ' + A.Cod_Asignatura) [Referencia], 
+	select (Cod_Facultad + ' - ' + Cod_Asignatura) [Referencia], 
 	Asignatura [Curso], 
-	Seccion, (Edificio + ' - ' + Num_Aula) [Aula], 
+	Seccion, (Edificio + ' - ' + Aula) [Aula], 
+	(codigo_empleado + ' - ' + Nombre1 + ' ' + Nombre2 + ' ' + Apellido1 + ' ' + isnull(Apellido2,'') + ' ' ) [Empleado], 
+	--isnull: comando de condición para insertar valor nulo como vacío o '' (este solo es de un valor y su reemplazo)
 	Lunes [L], Martes [M], Miercoles [M], Jueves [J], Viernes [V], Sabado [S]
 	from Asistencia A
 	join Clases C
-	on A.Cod_Asignatura = C.Cod_Asignatura
+	on A.ID_Clase = C.ID_Clase
 	join Sitio S
 	on A.ID_Sitio = S.ID_Sitio
 	join Empleados E
-	on A.codigo_empleado = E.codigo_empleado
+	on A.ID_Empleado = E.ID_Empleado
 	join Toma_Asistencia TA
 	on A.ID_Asistencia = TA.ID_Asistencia
+	join Nombres_Completos nc
+	on E.ID_Empleado = nc.ID_Empleado
 end
+exec PA_Admin
 go
 
+/*
 create proc PA_Asistencia_Superv -- Toma de Asistencia para supervisor
 -- Declarar variables ___ para usarlas en el...
 with encryption
@@ -150,8 +158,9 @@ begin
 	on A.ID_Asistencia = TA.ID_Asistencia
 end
 go
+*/
 
-create proc PA_Asistencia_Doc -- Toma de Asistencia para docente
+/*create proc PA_Asistencia_Doc -- Toma de Asistencia para docente
 @Clase varchar(55),
 @Seccion varchar (4),
 @L bit,
@@ -175,39 +184,10 @@ begin
 	where Asignatura = @Clase and Seccion = @Seccion and 
 	Lunes = @L and Martes = @M and Miercoles = @X and Jueves = @J and Viernes = @V and Sabado = @S
 end
-go
 exec PA_Asistencia_Doc 'Clase1', '0701', 0, 0, 0, 0, 0, 0	
+*/
 
-	--Datos a DGVADMIN
-create procedure PA_Admin
-with encryption 
-as
-begin
-	select
-	(C.Cod_Facultad + ' ' + A.Cod_Asignatura) AS Referencia, 
-        C.Asignatura AS Curso, 
-        S.Seccion, 
-        (S.Edificio + ' - ' + CAST(S.Num_Aula AS VARCHAR)) AS Aula, 
-        TA.Lunes AS L, 
-        TA.Martes AS M, 
-        TA.Miercoles AS M, 
-        TA.Jueves AS J, 
-        TA.Viernes AS V, 
-        TA.Sabado AS S
-	from 
-	Asistencia A
-    INNER JOIN 
-        Clases C ON A.Cod_Asignatura = C.Cod_Asignatura
-    INNER JOIN 
-        Sitio S ON A.ID_Sitio = S.ID_Sitio
-    INNER JOIN 
-        Empleados E ON A.codigo_empleado = E.codigo_empleado
-    INNER JOIN 
-        Toma_Asistencia TA ON A.ID_Asistencia = TA.ID_Asistencia;
-END
-GO
-
-	
+/*
 CREATE PROCEDURE PA_Justificaciones
 with encryption
 AS
@@ -224,8 +204,9 @@ BEGIN
     WHERE A.Justificacion IS NOT NULL
 END
 GO
+*/
 
-drop PROCEDURE PA_Reponer_Deca
+/*create PROCEDURE PA_Reponer_Deca
 with encryption
 AS
 BEGIN
@@ -240,3 +221,4 @@ BEGIN
     JOIN Empleados E ON A.codigo_empleado = E.codigo_empleado
     WHERE Fecha_Reposicion IS NOT NULL
 END
+*/
