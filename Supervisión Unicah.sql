f71ff92e0bd2634f65a86372478ba5e33288f903
Byron-Ing <byrd_riverat42@unicah.edu>
2025-03-19 15:32:00 -0600

    Cambio en PA_Admin y tabla de asistencia.sql
    No borrar base de datos por agregar asistencia

--- Supervisión Unicah.sql
+++ Supervisión Unicah.sql
@@ -145,6 +145,31 @@ drop column Nombre3
 alter table Migración$DatosExternos_3
 drop column Nombre4
 go
+
+create table Asistencia -- ¿Habrá tantos inserts con un checkbox desmarcado?
+( 
+	ID_Asistencia int identity primary key,
+	ID_Clase int foreign key references Clases (ID_Clase),
+    	ID_Sitio int foreign key references Sitio (ID_Sitio),
+	ID_Empleado int foreign key references Nombres_Completos(ID_Empleado),
+	Fecha date,
+	Observacion nvarchar(150), 
+	Fecha_Reposicion date,
+	Presente bit
+) 
+go
+
+insert into Asistencia (ID_Empleado, ID_Clase, ID_Sitio)
+select ID_Empleado, ID_Clase, ID_Sitio from Migración$DatosExternos_3
+go
+
+update Asistencia
+set Presente = 0
+go
+
+update Asistencia
+set Fecha = getdate()
+go
 	
 -- Procedimientos Almacenados
 create PROCEDURE PA_Login
@@ -159,31 +184,6 @@ BEGIN
 	WHERE codigo_empleado = @usuario --AND contraseña = @contrasena
 END
 GO
-
-create proc PA_Admin 
-with encryption
-as 
-begin
-	select (Cod_Facultad + ' - ' + Cod_Asignatura) [Referencia], 
-	Asignatura [Curso], 
-	Seccion, (Edificio + ' - ' + Aula) [Aula], 
-	(codigo_empleado + ' - ' + Nombre1 + ' ' + Nombre2 + ' ' + Apellido1 + ' ' + isnull(Apellido2,'') + ' ' ) [Empleado], 
-	--isnull: comando de condición para insertar valor nulo como vacío o '' (este solo es de un valor y su reemplazo)
-	Lunes [L], Martes [M], Miercoles [M], Jueves [J], Viernes [V], Sabado [S]
-	from Asistencia A
-	join Clases C
-	on A.ID_Clase = C.ID_Clase
-	join Sitio S
-	on A.ID_Sitio = S.ID_Sitio
-	join Empleados E
-	on A.ID_Empleado = E.ID_Empleado
-	join Toma_Asistencia TA
-	on A.ID_Asistencia = TA.ID_Asistencia
-	join Nombres_Completos nc
-	on E.ID_Empleado = nc.ID_Empleado
-end
-exec PA_Admin
-go
 
 create proc PA_Contra
 @Usuario varchar(4),
@@ -202,28 +202,60 @@ begin
 end
 go
 
-/*
-create proc PA_Asistencia_Superv -- Toma de Asistencia para supervisor
--- Declarar variables ___ para usarlas en el...
+create proc PA_Supervisor -- Toma de Asistencia para supervisor
+with encryption
+as                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+begin  
+	select (Nombre1 + ' ' + Nombre2 + ' ' + Apellido1 + ' ' + isnull(Apellido2,'')) [Docente], 
+	Asignatura,
+	Seccion, 
+	Aula,
+	Edificio
+	from Asistencia A
+	join Clases C
+	on A.ID_Clase = C.ID_Clase
+	join Sitio S
+	on A.ID_Sitio = S.ID_Sitio
+	join Empleados E 
+	on A.ID_Empleado = E.ID_Empleado
+	join Nombres_Completos NC
+	on E.ID_Empleado = NC.ID_Empleado
+	where codigo_empleado != '037'
+end
+go
+
+create proc PA_Asistencia
+@idEmpleado int
 with encryption
 as 
 begin
-	select Asignatura, 
-	(nombre_empleado + ' ' + apellido_empleado) [Docente], 
-	Seccion, 
-	Lunes [L], Martes [M], Miercoles [M], Jueves [J], Viernes [V], Sabado [S]
+	SELECT Fecha FROM Asistencia WHERE ID_Empleado = @idEmpleado 
+end
+go
+
+--En asistencia insertan y luego actualizan
+create proc PA_Admin -- Para el Admin
+with encryption
+as 
+begin
+	select (Cod_Facultad + ' - ' + Cod_Asignatura) [Referencia], 
+	Asignatura [Curso], 
+	Seccion, (Edificio + ' - ' + Aula) [Aula], 
+	(codigo_empleado + ' - ' + Nombre1 + ' ' + Nombre2 + ' ' + Apellido1 + ' ' + isnull(Apellido2,'')) [Empleado]
+	--isnull: comando de condición para insertar valor nulo como vacío o '' (este solo es de un valor y su reemplazo)
 	from Asistencia A
 	join Clases C
-	on A.Cod_Asignatura = C.Cod_Asignatura
+	on A.ID_Clase = C.ID_Clase
 	join Sitio S
 	on A.ID_Sitio = S.ID_Sitio
 	join Empleados E
-	on A.codigo_empleado = E.codigo_empleado
-	join Toma_Asistencia TA
-	on A.ID_Asistencia = TA.ID_Asistencia
+	on A.ID_Empleado = E.ID_Empleado
+	join Nombres_Completos nc
+	on E.ID_Empleado = nc.ID_Empleado
+	where codigo_empleado != '037'
 end
 go
-*/
+exec PA_Admin  
 
 /*create proc PA_Asistencia_Doc -- Toma de Asistencia para docente
 @Clase varchar(55),
