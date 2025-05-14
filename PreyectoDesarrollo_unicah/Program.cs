b16f7bf810806ca01b9b05060af34a0ef8e4aa58
Byron <byrd_riverat42@unicah.edu>
2025-05-14 16:57:22 -0600

    Add files via upload

--- PreyectoDesarrollo_unicah/Program.cs
+++ PreyectoDesarrollo_unicah/Program.cs
@@ -8,9 +8,24 @@ namespace PreyectoDesarrollo_unicah
         ///  The main entry point for the application.
         /// </summary>
 
+        public static string RolExe = "desconocido";
+
         [STAThread]
         static void Main(string[] args)
         {
+            string Rol = Path.GetFileName(Application.ExecutablePath).ToLower();
+            MessageBox.Show("Ejecutable detectado: " + Rol);
+
+            if (Rol.Contains("administrador"))
+                RolExe = "administrador";
+            else if (Rol.Contains("supervisor"))
+                RolExe = "supervisor";
+            else if (Rol.Contains("decano"))
+                RolExe = "decano";
+            else if (Rol.Contains("docente"))
+                RolExe = "docente";
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
